2ccb23fcc71e0d9ca815e375a252e28ca5daa2fe
Stazz0 <devopstazz@gmail.com>
2024-03-08 01:41:21 +0530

    Adding the essentail files

---
+++ RO-Crate Analyzer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+from rocrate.rocrate import ROCrate
+from rocrate.model.person import Person
+from rocrate.model.contextentity import ContextEntity
+import json
+import os
+
+def Ro_Crate_Analysis(ro_crate_file ): #or ro_crate_file_path
+
