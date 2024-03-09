0da1eed69c82653eafc63bb686cb90c6dc8ae12b
Stazz0 <devopstazz@gmail.com>
2024-03-10 01:28:34 +0530

    RO-Crate-Analyzer-update

--- RO-Crate Analyzer.py
+++ RO-Crate Analyzer.py
@@ -6,5 +6,21 @@ from rocrate.model.contextentity import ContextEntity
 import json
 import os
 
-def Ro_Crate_Analysis(ro_crate_file ): #or ro_crate_file_path
+def analyze_ro_crate(rocrate_path):
+    crate = ROCrate(rocrate_path)
 
+    with open(os.path.join(rocrate_path, 'ro-crate-metadata.json'), 'r') as f:
+        metadata = json.load(f)
+
+    dependencies = metadata.get('softwareDependencies', [])
+    os_requirements = metadata.get('operatingSystem', None)
+
+    print("Software Dependencies:")
+    for dep in dependencies:
+        print(dep['name'], dep.get('version', 'Unspecified'))  # Improved output
+
+    print("Operating System Requirements:")
+    if os_requirements:
+        print(os_requirements)
+    else:
+        print("Operating system requirements not explicitly specified")
